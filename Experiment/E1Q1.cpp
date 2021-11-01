e44ab601e7604f0107b45a44ebd63d7acaf3d3e0
Yuzi <2205816944@qq.com>
2021-11-01 18:29:46 +0800

    E1Q1

---
+++ Experiment/E1Q1.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+
+typedef struct E1Q1
+{
+    int data;
+    E1Q1 *next;
+};
+
+typedef struct E1Q1_bilateral
+{
+    int data;
+    E1Q1_bilateral *next;
+    E1Q1_bilateral *back;
+};
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    E1Q1 *queue = new E1Q1;
+    queue->data = 0;
+    E1Q1 *p = queue;
+    for (size_t i = 0; i < 10; i++)
+    {
+        p->next = new E1Q1;
+        p->next->data = (rand() % (1000 - 100)) + 100;
+        p = p->next;
+    }
+    p->next = new E1Q1;
+    p->next->data = 0;
+    p->next->next = NULL;
+
+    p = queue->next;
+    while (p->next)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+
+    cout << endl;
+    cout << "请输入一个整数： ";
+    int a;
+    int place = 1;
+    cin >> a;
+    p = queue->next;
+    while (p->next)
+    {
+        if (p->data == a)
+        {
+            cout << "该数在表中，是第" << place << "位" << endl;
+            break;
+        }
+        place++;
+        p = p->next;
+    }
+    if (!p->next)
+    {
+        cout << "该数不在表中！！" << endl;
+    }
+
+    while (1)
+    {
+        cout << "请输入一个数字和要插入的位置： ";
+        cin >> a >> place;
+        if (place < 1 || place > 11)
+        {
+            cout << "输入位置非法！请重试！" << endl;
+            continue;
+        }
+        p = queue;
+        for (size_t i = 1; i < place; i++)
+        {
+            p = p->next;
+        }
+        E1Q1 *q = new E1Q1;
+        q->next = p->next;
+        q->data = a;
+        p->next = q;
+
+        p = queue->next;
+        while (p->next)
+        {
+            cout << p->data << " ";
+            p = p->next;
+        }
+        break;
+    }
+    cout << endl;
+
+    E1Q1_bilateral *b = new E1Q1_bilateral;
+    E1Q1_bilateral *q = new E1Q1_bilateral;
+    b->next = q;
+    q->back = b;
+    b->back = NULL;
+    p = queue->next;
+    while (p->next)
+    {
+        q->data = p->data;
+        q->next = new E1Q1_bilateral;
+        q->next->back = q;
+        q = q->next;
+        p = p->next;
+    }
+    q = q->back;
+    cout << "倒序输出是：" << endl;
+    while (q->back)
+    {
+        cout << q->data << " ";
+        q = q->back;
+    }
+    cout << endl;
+}
