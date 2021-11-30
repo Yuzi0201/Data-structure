d8c2d74e9722b3dbe2ddfb81abc9b29ab9b3e2ce
Yuzi <2205816944@qq.com>
2021-11-30 13:29:02 +0800

    E3_最小生成树

--- Experiment/E3.cpp
+++ Experiment/E3.cpp
@@ -57,6 +57,7 @@ int LocateVex(ALGraph G, int vertex)
     for (size_t i = 0; i < G.vexnum; i++)
         if (G.vertices[i].data == vertex)
             return i;
+    return -1;
 }
 
 int FirstAdjVex(ALGraph *G, int v);
@@ -65,7 +66,7 @@ int NextAdjVex(ALGraph *G, int v, int w);
 void DFS(ALGraph *G, int v) //Depth First Search
 {
     static bool visited[7] = {false};
-    cout << v;
+    cout << v << " ";
     visited[v] = 1;
     for (int w = FirstAdjVex(G, v); w >= 0; w = NextAdjVex(G, v, w))
     {
@@ -91,7 +92,7 @@ int NextAdjVex(ALGraph *G, int v, int w)
 
 void BFS(ALGraph *G, int v) //Breadth First Search
 {
-    cout << v;
+    cout << v << " ";
     static bool visited[7] = {false};
     visited[v] = true; //第v个（也就是第0个节点已访问）
     queue<int> q;
@@ -103,10 +104,111 @@ void BFS(ALGraph *G, int v) //Breadth First Search
         for (int w = FirstAdjVex(G, u); w >= 0; w = NextAdjVex(G, u, w))
             if (!visited[w])
             {
-                cout << w;
+                cout << w << " ";
                 visited[w] = true;
                 q.push(w);
             }
+    }
+}
+
+typedef struct
+{
+    int head;
+    int tail;
+    int lowcost;
+    bool selected = false;
+} Edge[9]; //克鲁斯卡尔算法的辅助数组，记录边的信息
+
+ArcNode *NextVex(ALGraph *G, int v, int w)
+{
+    ArcNode *p = G->vertices[v].firstarc;
+    while (p->adjvex != w)
+        p = p->nextarc;
+    if (!p->nextarc)
+        return nullptr; //下一条边是空的了
+    return p->nextarc;  //此时p就是w的节点，返回下一边
+}
+
+void InitAndSortEdge(ALGraph *G, Edge e[])
+{
+    int v = 0;
+    static bool visited[7][7] = {false}; //边的访问矩阵
+    int vex = 0;
+    queue<int> q;
+    q.push(v);
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+        for (ArcNode *v = G->vertices[u].firstarc; v; v = NextVex(G, u, v->adjvex))
+            if (!visited[v->adjvex][u] && !visited[u][v->adjvex]) //此边指向的点到u和u到此边指向的点都未访问过
+            {
+                //cout << v->weight << " ";//for debug
+                e[vex]->lowcost = v->weight;
+                e[vex]->head = u;
+                e[vex]->tail = v->adjvex;
+                vex++;
+                visited[u][v->adjvex] = true; //u到此边指向的点已访问
+                q.push(v->adjvex);
+            }
+    } //↑提取边的权重的部分
+    for (size_t i = 0; i < 9 - 1; i++)
+    {
+        for (size_t j = 0; j < 9 - 1 - i; j++)
+        {
+            if (e[j]->lowcost > e[j + 1]->lowcost)
+                swap(e[j], e[j + 1]);
+        }
+    } //按照边的权重进行冒泡排序
+}
+
+bool All_connected(bool connected[])
+{
+    for (size_t i = 0; i < 7; i++)
+    {
+        if (!connected[i])
+            return false;
+    }
+    return true;
+}
+
+void MiniSpanTree_Kruskal(ALGraph *G)
+{
+    Edge e[9];
+    InitAndSortEdge(G, e);                 //提取所有边的权重并排序
+    int Vexset[7] = {0, 1, 2, 3, 4, 5, 6}; //表示连通分量，初始时各顶点自成连通分量
+    for (size_t i = 0; i < 9; i++)
+    {
+        int vs1 = Vexset[e[i]->head];
+        int vs2 = Vexset[e[i]->tail];
+        if (Vexset[e[i]->head] != Vexset[e[i]->tail]) //如果两个顶点属于不同的连通分量，则连接
+        {
+            e[i]->selected = true;
+            //cout << e[i]->head << " " << e[i]->tail << "    ";//for debug
+            for (size_t j = 0; j < 7; j++) //将尾顶点所属的连通分量的所有点合并入头顶点的连通分量
+                if (Vexset[j] == vs2)
+                    Vexset[j] = vs1;
+        }
+    }
+    //cout << endl;
+    int head = e[0]->head;
+    cout << head;
+    bool visted[7] = {false};
+    visted[head] = true;
+    while (!All_connected(visted))
+    {
+        for (size_t i = 0; i < 9; i++)
+        {
+            if (e[i]->selected && (e[i]->head == head || e[i]->tail == head))
+            {
+                int next = e[i]->head == head ? e[i]->tail : e[i]->head; //确定哪一端是线头
+                cout << "--" << e[i]->lowcost << "--> " << next;
+                visted[next] = true;
+                head = next;
+                e[i]->selected = false;
+                break;
+            }
+        }
     }
 }
 
@@ -135,4 +237,5 @@ int main()
     printf("\n广度优先遍历为：\n");
     BFS(G, v);
     printf("\n最小生成树为：\n");
+    MiniSpanTree_Kruskal(G);
 }
